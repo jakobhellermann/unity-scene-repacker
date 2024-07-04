ef9eaefb4d630f8e97b738e0e1f3efce1fc6a0a4
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-07-04 17:11:13 +0200

    tmp

--- src/cli_filterdown.py
+++ src/cli_filterdown.py
@@ -3,14 +3,15 @@ from pathlib import Path
 import json
 
 from repack import repack_scene_bundle
+from prune import prune, get_root_objs
 
 scene_map = json.load(open("in/scenes.json", "r"))
 monsters = [
     ("A9_S1_Remake_4wei", "A9_S1/Room/Prefab/A9_MonsterCandidate/StealthGameMonster_GunBoyElite"),
-    (
-        "A10_S5_Boss_Jee",
-        "A10S5/Room/Boss And Environment Binder/General Boss Fight FSM Object 姬 Variant/FSM Animator/LogicRoot/---Boss---/BossShowHealthArea/StealthGameMonster_Boss_Jee",
-    ),
+    # (
+    #    "A10_S5_Boss_Jee",
+    #    "A10S5/Room/Boss And Environment Binder/General Boss Fight FSM Object 姬 Variant/FSM Animator/LogicRoot/---Boss---/BossShowHealthArea/StealthGameMonster_Boss_Jee",
+    # ),
 ]
 
 
@@ -21,13 +22,23 @@ level_names = [name for name, _ in monsters]
 levels = [f"level{scene_map[name]}" for name, _ in monsters]
 paths = [str(project.joinpath(level)) for level in levels]
 
+env = Environment(*paths)
+serialized_files = [env.files[path] for path in paths]
 
-e = Environment(*paths)
+for level_name, file in zip(level_names, serialized_files):
+    level_monsters = [path for scene, path in monsters if scene == level_name]
+    prune(file, level_monsters)
 
-new_bundle = repack_scene_bundle(dict(zip(level_names, map(lambda path: e.files[path], paths))))
+new_bundle = repack_scene_bundle(dict(zip(level_names, serialized_files)))
 
 
 if __name__ == "__main__":
     out_path.parent.mkdir(parents=True, exist_ok=True)
     with open(out_path, "wb") as f:
         f.write(new_bundle.save())
+
+    sanity = Environment(str(out_path)).file
+    s = sanity.files["BuildPlayer-A9_S1_Remake_4wei"]
+
+    for go in get_root_objs(s):
+        print(go.m_Transform.read().m_Father.__dict__)
--- src/prune.py
+++ src/prune.py
@@ -1,14 +1,84 @@
-from UnityPy.files import SerializedFile
+from collections.abc import Iterator
+from UnityPy.files import SerializedFile, ObjectReader
+from UnityPy.classes import Object, GameObject, Transform, PPtr
+from UnityPy.enums import ClassIDType
+from collections import deque
 
 
-def prune(scene: SerializedFile):
-    print(scene)
+def prune(scene: SerializedFile, keep_paths: list[str]):
+    root_objs = list(get_root_objs(scene))
+    keep_new_root = [lookup_path(keep, root_objs) for keep in keep_paths]
+
+    include: set[int] = set()
+    queue: deque[Object] = deque([keep.reader for keep in keep_new_root])
+
+    while queue:
+        node = queue.popleft()
+
+        include.add(node.path_id)
+
+        # if isinstance(node, GameObject):
+        # print(node.m_Components)
+        for reachable in iterate_visible(node):
+            if reachable.path_id not in include:
+                queue.append(reachable)
+
+    old_objects = scene.objects
+    new_objects = {key: old_objects[key] for key in include}
+    scene.objects = new_objects
+
+    # scene.objects = {}
+
+    for keep in keep_new_root:
+        invalid_parent = keep.m_Transform.get_obj()
+        tt = invalid_parent.read_typetree()
+        tt["m_Father"] = {"m_FileID": 0, "m_PathID": 0}
+        invalid_parent.save_typetree(tt)
 
 
-# def get_root_objects(file: SerializedFile) -> Iterator[GameObject]:
-#     for obj in file.objects.values():
-#         if obj.class_id == 4:
-#             transform: Transform = obj.read()
-#             parent = transform.m_Father.get_obj()
-#             if parent is None:
-#                 yield transform.m_GameObject.read()
+def iterate_visible(obj: ObjectReader):
+    if isinstance(obj, PPtr):
+        obj = obj.get_obj()
+
+    if obj.type == ClassIDType.GameObject:
+        obj: GameObject = obj.read()
+        yield from obj.m_Components
+    elif obj.type == ClassIDType.Transform:
+        obj: Transform = obj.read()
+        yield obj.m_GameObject
+        yield from obj.m_Children
+
+
+def lookup_path_in(path: list[str], go: GameObject):
+    current: Transform = go.m_Transform.read()
+
+    while path:
+        segment = path.pop(0)
+
+        for child in current.m_Children:
+            child: Transform = child.read()
+            if child.m_GameObject.read().name == segment:
+                current = child
+                break
+        else:
+            raise Exception(f"path '{'/'.join(path)}' not found at {segment}")
+
+    return current.m_GameObject.read()
+
+
+def lookup_path(path: str, root_objs: list[GameObject]):
+    root, *rest = path.split("/")
+
+    root_obj = next(filter(lambda obj: obj.name == root, root_objs), None)
+    assert root_obj is not None
+
+    return lookup_path_in(rest, root_obj)
+
+
+def get_root_objs(file: SerializedFile) -> Iterator[GameObject]:
+    for obj in file.objects.values():
+        if obj.class_id == 4:
+            transform: Transform = obj.read()
+            parent = transform.m_Father.get_obj()
+            if parent is None:
+                yield transform.m_GameObject.read()
