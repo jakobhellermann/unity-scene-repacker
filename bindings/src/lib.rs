3c49aa625d6e0fe3d9b174a28807c97ef60b56d0
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-06-27 20:42:17 +0200

    bindings: update to asset bundles, clean up build.rs

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -2,7 +2,7 @@ use std::ffi::{CStr, CString, c_char, c_int};
 use std::io::Cursor;
 use std::path::Path;
 
-use anyhow::{Context as _, Result};
+use anyhow::{Context as _, Result, bail};
 use indexmap::IndexMap;
 use unity_scene_repacker::Stats;
 use unity_scene_repacker::rabex::UnityVersion;
@@ -16,6 +16,11 @@ pub struct CStats {
     pub objects_after: c_int,
 }
 
+enum Mode {
+    SceneBundle = 0,
+    AssetBundle = 1,
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn export(
     name: *const c_char,
@@ -25,13 +30,14 @@ pub extern "C" fn export(
     bundle_size: *mut c_int,
     bundle_data: *mut *mut u8,
     stats_ret: *mut CStats,
+    mode: u8,
 ) {
     unsafe {
         let name = CStr::from_ptr(name);
         let game_dir = CStr::from_ptr(game_dir);
         let preload_json = CStr::from_ptr(preload_json);
 
-        let result = export_inner(name, game_dir, preload_json);
+        let result = export_inner(name, game_dir, preload_json, mode);
         match result {
             Ok((stats, data)) => {
                 *bundle_size = data.len() as c_int;
@@ -64,46 +70,76 @@ pub extern "C" fn free_array(len: c_int, data: *mut u8) {
     };
 }
 
-fn export_inner(name: &CStr, game_dir: &CStr, preload_json: &CStr) -> Result<(Stats, Vec<u8>)> {
+fn export_inner(
+    name: &CStr,
+    game_dir: &CStr,
+    preload_json: &CStr,
+    mode: u8,
+) -> Result<(Stats, Vec<u8>)> {
     let name = name.to_str()?;
     let game_dir = Path::new(game_dir.to_str()?);
     let preload_json = preload_json.to_str()?;
+    let mode = match mode {
+        0 => Mode::SceneBundle,
+        1 => Mode::AssetBundle,
+        _ => bail!("Expected 0=SceneBundle or 1=AssetBundle, got {mode}"),
+    };
 
-    let tpk = TpkTypeTreeBlob::embedded();
-    let tt = TypeTreeCache::new(TpkTypeTreeBlob::embedded());
+    let tpk_raw = TpkTypeTreeBlob::embedded();
+    let tpk = TypeTreeCache::new(TpkTypeTreeBlob::embedded());
 
     let compression = CompressionType::None;
 
     let preloads: IndexMap<String, Vec<String>> =
         serde_json::from_str(&preload_json).context("error parsing the objects json")?;
 
-    let temp_dir = Path::new("/tmp/todo");
+    let temp_dir = Path::new("/tmp/todo"); // unused for hollowknight
     let disable = true;
 
     let unity_version: UnityVersion = "2020.2.2f1".parse().unwrap();
 
     let mut repack_scenes =
-        unity_scene_repacker::repack_scenes(&game_dir, preloads, &tt, &temp_dir, disable)?;
-
-    let (stats, header, files) = unity_scene_repacker::pack_to_scene_bundle(
-        name,
-        &tpk,
-        &tt,
-        unity_version,
-        repack_scenes.as_mut_slice(),
-    )
-    .context("trying to repack bundle")?;
+        unity_scene_repacker::repack_scenes(&game_dir, preloads, &tpk, &temp_dir, disable)?;
 
     let mut out = Cursor::new(Vec::new());
-    bundlefile::write_bundle_iter(
-        &header,
-        &mut out,
-        CompressionType::Lz4,
-        compression,
-        files
-            .into_iter()
-            .map(|(name, file)| Ok((name, Cursor::new(file)))),
-    )?;
+
+    let stats = match mode {
+        Mode::SceneBundle => {
+            let (stats, header, files) = unity_scene_repacker::pack_to_scene_bundle(
+                name,
+                &tpk_raw,
+                &tpk,
+                unity_version,
+                repack_scenes.as_mut_slice(),
+            )
+            .context("trying to repack bundle")?;
+
+            bundlefile::write_bundle_iter(
+                &header,
+                &mut out,
+                CompressionType::None,
+                compression,
+                files
+                    .into_iter()
+                    .map(|(name, file)| Ok((name, Cursor::new(file)))),
+            )?;
+
+            stats
+        }
+        Mode::AssetBundle => {
+            let stats = unity_scene_repacker::pack_to_asset_bundle(
+                game_dir,
+                &mut out,
+                name,
+                &tpk_raw,
+                &tpk,
+                unity_version,
+                repack_scenes,
+                compression,
+            )?;
+            stats
+        }
+    };
 
     Ok((stats, out.into_inner()))
 }
--- build.rs
+++ build.rs
@@ -1,13 +1,4 @@
 fn main() {
-    /*println!(
-        "cargo:rustc-link-search=native=/home/jakob/dev/unity/TypeTreeGeneratorAPI/TypeTreeGeneratorAPI/bin/Release/net10.0/linux-x64/publish"
-    );*/
-    // println!("cargo:rustc-link-search=native=/usr/local/lib");
-
     println!("cargo:rustc-link-search=native=/home/jakob/dev/unity/unity-scene-repacker");
     println!("cargo:rustc-link-lib=TypeTreeGeneratorAPI");
-
-    // println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../lib");
-    // println!("cargo:rustc-link-arg=-Wl,-rpath,/usr/lib/libTypeTreeGeneratorAPIÖ.so");
-    // println!("cargo:rustc-link-search=native=.");
 }
