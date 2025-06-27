e4f1c11cba2fca3dba4cca77303a0be642586bc1
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-06-27 15:24:50 +0200

    perf: parallelism for object data remapping

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,15 @@ use memmap2::Mmap;
 use rabex::files::bundlefile::{
     self, BundleFileBuilder, BundleFileHeader, BundleFileReader, CompressionType, ExtractionConfig,
 };
-use rabex::files::serializedfile::SerializedType;
 use rabex::files::serializedfile::builder::SerializedFileBuilder;
+use rabex::files::serializedfile::{ObjectInfo, SerializedType};
 use rabex::files::{SerializedFile, serializedfile};
 use rabex::objects::pptr::{FileId, PPtr, PathId};
 use rabex::objects::{ClassId, ClassIdType};
 use rabex::tpk::TpkTypeTreeBlob;
 use rabex::typetree::{TypeTreeNode, TypeTreeProvider};
 use rabex::{UnityVersion, serde_typetree};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use rustc_hash::{FxHashMap, FxHashSet};
 use std::borrow::Cow;
 use std::collections::BTreeSet;
@@ -502,9 +503,9 @@ pub fn pack_to_asset_bundle(
     writer: impl Write + Seek,
     bundle_name: &str,
     tpk_blob: &TpkTypeTreeBlob,
-    tpk: &impl TypeTreeProvider,
+    tpk: &(impl TypeTreeProvider + Send + Sync),
     unity_version: UnityVersion,
-    scenes: &mut [RepackScene],
+    scenes: Vec<RepackScene>,
 ) -> Result<()> {
     let common_offset_map = serializedfile::build_common_offset_map(tpk_blob, unity_version);
 
@@ -519,65 +520,97 @@ pub fn pack_to_asset_bundle(
 
     let mut container = IndexMap::new();
 
-    for scene in scenes {
-        let serialized = &mut scene.serialized;
-        let file = File::open(&scene.serialized_path)?;
-        let mut data = Cursor::new(unsafe { Mmap::map(&file)? });
+    let intermediate = scenes
+        .into_iter()
+        .map(|mut scene| {
+            let serialized = &mut scene.serialized;
+            let file = File::open(&scene.serialized_path)?;
+            let mut data = Cursor::new(unsafe { Mmap::map(&file)? });
 
-        assert_eq!(serialized.m_bigIDEnabled, None);
-        assert!(serialized.m_RefTypes.as_ref().is_some_and(|x| x.is_empty()));
+            assert_eq!(serialized.m_bigIDEnabled, None);
+            assert!(serialized.m_RefTypes.as_ref().is_some_and(|x| x.is_empty()));
 
-        let mb_types = serialized
-            .objects_of::<MonoBehaviour>(tpk)?
-            .map(|mb_info| {
-                let mb = mb_info.read(&mut data)?;
-                let script = env.deref_read(mb.m_Script, serialized, &mut data)?;
-                let full_ty =
-                    generator_cache.generate(&script.m_AssemblyName, &script.full_name())?;
-                Ok((mb_info.info.m_PathID, full_ty))
-            })
-            .collect::<Result<FxHashMap<_, _>>>()?;
+            let mb_types = prepare_monobehaviour_types(
+                &scene.scene_name,
+                tpk,
+                &env,
+                &generator_cache,
+                serialized,
+                &mut data,
+            )?;
 
-        serialized.modify_objects(|objects| {
-            objects.retain(|obj| scene.keep_objects.contains(&obj.m_PathID))
-        });
+            serialized.modify_objects(|objects| {
+                objects.retain(|obj| scene.keep_objects.contains(&obj.m_PathID))
+            });
 
-        let mut path_id_remap = FxHashMap::default();
+            let mut remap_path_id = FxHashMap::default();
 
-        for obj in serialized.objects() {
-            builder.get_next_path_id();
-            path_id_remap.insert(obj.m_PathID, builder.get_next_path_id());
-        }
-
-        for (transform, scene_path) in scene.roots.iter().copied().zip(scene.scene_paths.iter()) {
-            let transform = serialized
-                .get_object::<Transform>(transform, tpk)?
-                .read(&mut data)?;
-
-            let mut go = transform.m_GameObject;
-            assert!(go.is_local());
-            if let Some(replacement) = path_id_remap.get(&go.m_PathID) {
-                go.m_PathID = *replacement;
+            for obj in serialized.objects() {
+                builder.get_next_path_id();
+                remap_path_id.insert(obj.m_PathID, builder.get_next_path_id());
             }
 
-            let info = AssetInfo {
-                asset: go.untyped(),
-                ..Default::default()
-            };
-            let path = format!("{}/{}.prefab", scene.scene_name, scene_path).to_lowercase();
+            for (transform, scene_path) in scene.roots.iter().copied().zip(scene.scene_paths.iter())
+            {
+                let transform = serialized
+                    .get_object::<Transform>(transform, tpk)?
+                    .read(&mut data)?;
 
-            container.insert(path, info);
+                let mut go = transform.m_GameObject;
+                assert!(go.is_local());
+                if let Some(replacement) = remap_path_id.get(&go.m_PathID) {
+                    go.m_PathID = *replacement;
+                }
+
+                let info = AssetInfo {
+                    asset: go.untyped(),
+                    ..Default::default()
+                };
+                let path = format!("{}/{}.prefab", scene.scene_name, scene_path).to_lowercase();
+
+                container.insert(path, info);
+            }
+
+            let (remap_file_id, remap_types) = add_remapped_scene_header(&mut builder, serialized)?;
+
+            Ok((
+                scene,
+                data,
+                mb_types,
+                remap_file_id,
+                remap_path_id,
+                remap_types,
+            ))
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    let objects = intermediate
+        .into_par_iter()
+        .map(
+            |(mut scene, data, mb_types, remap_file_id, remap_path_id, remap_types)| {
+                add_remapped_scene(
+                    &scene.scene_name,
+                    tpk,
+                    &builder.serialized,
+                    scene.serialized.take_objects(),
+                    data.get_ref(),
+                    scene.replacements,
+                    mb_types,
+                    remap_file_id,
+                    remap_path_id,
+                    remap_types,
+                )
+                .collect::<Vec<_>>()
+            },
+        )
+        .collect::<Vec<Vec<Result<_>>>>();
+
+    objects.into_iter().try_for_each(|objects| -> Result<_> {
+        for obj in objects {
+            builder.objects.push(obj?);
         }
-
-        add_remapped_scene(
-            &mut builder,
-            serialized,
-            data.get_ref(),
-            &mut scene.replacements,
-            mb_types,
-            path_id_remap,
-        )?;
-    }
+        Ok(())
+    })?;
 
     let assetbundle_ty = tpk
         .get_typetree_node(AssetBundle::CLASS_ID, unity_version)
@@ -617,14 +650,113 @@ pub fn pack_to_asset_bundle(
     Ok(())
 }
 
+fn prepare_monobehaviour_types<'a, T: TypeTreeProvider>(
+    scene_name: &str,
+    tpk: &'a T,
+    env: &Environment<&T>,
+    generator_cache: &'a TypeTreeGeneratorCache,
+    serialized: &SerializedFile,
+    data: &mut (impl Read + Seek),
+) -> Result<FxHashMap<i64, &'a TypeTreeNode>> {
+    Ok(serialized
+        .objects_of::<MonoBehaviour>(tpk)?
+        .map(|mb_info| -> Result<_> {
+            let mb = mb_info.read(data)?;
+            let script = env
+                .deref_read(mb.m_Script, serialized, data)
+                .with_context(|| {
+                    format!(
+                        "In script {:?} of monoebehaviour {:?}",
+                        mb.m_Script, mb_info.info.m_PathID
+                    )
+                })?;
+
+            let assembly_name = match script.m_AssemblyName.ends_with(".dll") {
+                true => Cow::Borrowed(&script.m_AssemblyName),
+                false => Cow::Owned(format!("{}.dll", script.m_AssemblyName)),
+            };
+            let full_ty = generator_cache
+                .generate(&assembly_name, &script.full_name())
+                .with_context(|| {
+                    format!(
+                        "Reading script {} {}",
+                        script.m_AssemblyName,
+                        script.full_name()
+                    )
+                })
+                .with_context(|| format!("At object {}", mb_info.info.m_PathID))
+                .with_context(|| {
+                    format!("Could not generate type trees from MonoBehaviour in {scene_name}")
+                })?;
+
+            Ok((mb_info.info.m_PathID, full_ty))
+        })
+        .filter_map(|ty| match ty {
+            Ok(val) => Some(val),
+            Err(e) => {
+                log::error!("{e:?}");
+                None
+            }
+        })
+        .collect::<FxHashMap<_, _>>())
+}
+
 fn add_remapped_scene(
+    scene_name: &str,
+    tpk: &impl TypeTreeProvider,
+    serialized: &SerializedFile,
+    objects: Vec<ObjectInfo>,
+    data: &[u8],
+    mut replacements: FxHashMap<i64, Vec<u8>>,
+    mb_types: FxHashMap<i64, &TypeTreeNode>,
+    remap_file_id: FxHashMap<FileId, FileId>,
+    path_id_remap: FxHashMap<PathId, PathId>,
+    remap_types: FxHashMap<i32, i32>,
+) -> impl Iterator<Item = Result<(ObjectInfo, Cow<'static, [u8]>)>> {
+    objects.into_iter().map(move |mut obj| -> Result<_> {
+        obj.m_TypeID = remap_types[&obj.m_TypeID];
+
+        let tt = match mb_types.get(&obj.m_PathID) {
+            Some(ty) => ty,
+            // TODO: take types from file if they exist
+            None => &*serialized.get_typetree_for(&obj, &tpk)?,
+        };
+        let mut object_data = match replacements.remove(&obj.m_PathID) {
+            Some(owned) => Cow::Owned(owned),
+            None => {
+                let offset = obj.m_Offset as usize;
+                let size = obj.m_Size as usize;
+                Cow::Borrowed(&data[offset..offset + size])
+            }
+        };
+
+        let orig_path_id = obj.m_PathID;
+        obj.m_PathID = *path_id_remap.get(&obj.m_PathID).unwrap_or(&obj.m_PathID);
+
+        replace_pptrs_inplace_endianed(
+            object_data.to_mut().as_mut_slice(),
+            tt,
+            &path_id_remap,
+            &remap_file_id,
+            serialized.m_Header.m_Endianess,
+        )
+        .with_context(|| {
+            format!(
+                "Could not remap path IDs in bundle for {} in '{}':\n{}",
+                orig_path_id,
+                scene_name,
+                tt.dump_pretty()
+            )
+        })?;
+
+        Ok((obj, Cow::Owned(object_data.into_owned())))
+    })
+}
+
+fn add_remapped_scene_header(
     builder: &mut SerializedFileBuilder<impl TypeTreeProvider>,
     serialized: &mut SerializedFile,
-    data: &[u8],
-    replacements: &mut FxHashMap<i64, Vec<u8>>,
-    mb_types: FxHashMap<i64, &TypeTreeNode>,
-    path_id_remap: FxHashMap<i64, i64>,
-) -> Result<()> {
+) -> Result<(FxHashMap<FileId, FileId>, FxHashMap<i32, i32>)> {
     let mut remap_file_id = FxHashMap::default();
     for (i, external) in serialized.m_Externals.iter().enumerate() {
         let orig_file_id = i + 1;
@@ -652,39 +784,6 @@ fn add_remapped_scene(
         &mut serialized.m_Types,
         &mut builder.serialized.m_Types,
     );
-    let new_objects = serialized.take_objects();
-    let objects = new_objects.into_iter().map(|mut obj| -> Result<_> {
-        obj.m_TypeID = remap_types[&obj.m_TypeID];
 
-        let tt = match mb_types.get(&obj.m_PathID) {
-            Some(ty) => ty,
-            None => &*builder
-                .serialized
-                .get_typetree_for(&obj, builder.typetree_provider)?,
-        };
-        let mut object_data = match replacements.remove(&obj.m_PathID) {
-            Some(owned) => Cow::Owned(owned),
-            None => {
-                let offset = obj.m_Offset as usize;
-                let size = obj.m_Size as usize;
-                Cow::Borrowed(&data[offset..offset + size])
-            }
-        };
-
-        obj.m_PathID = *path_id_remap.get(&obj.m_PathID).unwrap_or(&obj.m_PathID);
-
-        replace_pptrs_inplace_endianed(
-            object_data.to_mut().as_mut_slice(),
-            tt,
-            &path_id_remap,
-            &remap_file_id,
-            serialized.m_Header.m_Endianess,
-        )?;
-
-        Ok((obj, Cow::Owned(object_data.into_owned())))
-    });
-    for object in objects {
-        builder.objects.push(object?);
-    }
-    Ok(())
+    Ok((remap_file_id, remap_types))
 }
